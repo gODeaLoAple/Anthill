4814bb100b5137cf085f13264f0be3c947ec8cde
Banan-dot <semke.danya@mail.ru>
2021-04-28 13:36:28 +0500

    Unused commit

--- src/main/java/client/domain/entities/ants/MasterAnt.java
+++ src/main/java/client/domain/entities/ants/MasterAnt.java
@@ -4,12 +4,4 @@ public class MasterAnt extends Ant{
     private final int health = 300;
     private final int speed = 15;
     private final int id = 1;
-
-    public void attack(){
-
-    }
-
-    public void protectColony(){
-
-    }
 }
--- src/main/java/client/domain/entities/ants/SlaveAnt.java
+++ src/main/java/client/domain/entities/ants/SlaveAnt.java
@@ -4,13 +4,4 @@ public class SlaveAnt extends Ant{
     private final int health = 100;
     private final int speed = 10;
     private final int id = 0;
-
-
-    public void buildAnthill(){
-
-    }
-
-    public void collectFood(){
-
-    }
 }
