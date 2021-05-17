dfa5a082060752bad31ab07446e7800c0cf6f0cf
gODealOAple <maksim.math@gmail.com>
2021-05-17 12:17:58 +0500

    Optimize ants drawer

--- src/main/java/client/ui/battle/drawers/forEachPlayer/AntDrawer.java
+++ src/main/java/client/ui/battle/drawers/forEachPlayer/AntDrawer.java
@@ -12,6 +12,8 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AntDrawer extends GameDrawer implements ForEachPlayerDrawer{
 
@@ -19,6 +21,7 @@ public class AntDrawer extends GameDrawer implements ForEachPlayerDrawer{
     private final int height;
     private final int width;
     private final ImageObserver obs;
+    private final Map<Integer, BufferedImage> angleToImage;
 
     public AntDrawer(Game game, ImageProvider provider) {
         super(game);
@@ -26,33 +29,32 @@ public class AntDrawer extends GameDrawer implements ForEachPlayerDrawer{
         antImage = provider.getAntImage();
         width = antImage.getWidth();
         height = antImage.getHeight();
+        angleToImage = createRotations();
     }
 
     @Override
     public void draw(Graphics2D graphics, Player player) {
-        var anthill = player.getAnthill();
-        var ants = anthill.getAnts();
-        var movement = anthill.getMovement();
-        for (var ant : ants) {
-            movement.moveAnt(ant);
-            rotateSprite(graphics, ant);
+        for (var ant : player.getAnthill().getAnts()) {
+            var position = ant.getPosition();
+            var vector = new Vector(position, ant.getDestination());
+            var angle = (int)(Math.toDegrees(vector.getAngle()));
+            graphics.drawImage(angleToImage.get(angle), position.x, position.y, 30, 30, obs);
         }
-
     }
 
-    private void rotateSprite(Graphics2D graphics, Ant ant){
-        var position = ant.getPosition();
-        var vector = new Vector(position, ant.getDestination());
-        var at = new AffineTransform();
-        var newImage = new BufferedImage(width, height, antImage.getType());
-        var newImageHeight = newImage.getHeight();
-        var newImageWidth = newImage.getWidth();
-        at.rotate(vector.getAngle() + Math.PI / 2, newImageWidth / 2.0, newImageHeight / 2.0);
-        at.translate((newImageWidth - width) / 2.0, (newImageHeight - height) / 2.0);
-        var op = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
-        try {
-            graphics.drawImage(op.filter(antImage, newImage), position.x, position.y, 30, 30, obs);
-        } catch (Exception ignored) {}
+    private Map<Integer, BufferedImage> createRotations() {
+        var map = new HashMap<Integer, BufferedImage>(360);
+        for (var i = -180; i < 180; i++) {
+            var at = new AffineTransform();
+            var newImage = new BufferedImage(width, height, antImage.getType());
+            var newImageHeight = newImage.getHeight();
+            var newImageWidth = newImage.getWidth();
+            at.rotate(Math.toRadians(i) + Math.PI / 2, newImageWidth / 2.0, newImageHeight / 2.0);
+            at.translate((newImageWidth - width) / 2.0, (newImageHeight - height) / 2.0);
+            var op = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+            map.put(i, op.filter(antImage, newImage));
+        }
+        return map;
     }
 
     @Override
